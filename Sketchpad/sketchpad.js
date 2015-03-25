31492096e3efcbad2ee87726ccb0d15d01ef6acc
Rob Pennington <robpenn002@gmail.com>
2015-03-25 14:50:19 -0500

    update sketchpad

--- Sketchpad/sketchpad.js
+++ Sketchpad/sketchpad.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
 	wrapper = $(".wrapper");
 	gridWidth = wrapper.width();
+	popDropDown("grid",2,32);
 	createGrid(12);
 	square.on("mouseenter", normalColor);
 });
 
 var normal = $("#normal");
 normal.on("click", function() {
-	resetGrid();
+	square.off();
+	square.css("background-color", "");
 	square.on("mouseenter", normalColor);
 });
 
 var random = $("#random");
 random.on("click", function() {
-	resetGrid();
+	square.off();
+	square.css("background-color", "");
 	square.on("mouseenter", function() {
 		$(this).css("background-color", randomColor());
 	}); 
@@ -21,31 +24,33 @@ random.on("click", function() {
 
 var tracer = $("#tracer");
 tracer.on("click", function() {
-	resetGrid();
+	square.off();
+	square.css("background-color", "");
 	square.on("mouseenter", boxTracer);
 });
 
-function resetGrid() {
-	var newSize = prompt("Enter a number between 2 and 128");
-		if (newSize >= 2 && newSize <= 128) {
-			// force grid size to be a factor of .wrapper width
-			while (gridWidth % newSize !== 0)
-				newSize--;
-			createGrid(newSize);
+function popDropDown(id, min, max) {
+	var i, select, option;
+	select = document.getElementById(id);
+	for (i = min; i <= max; i += 1) {
+		if (gridWidth % i  === 0) {
+			option = document.createElement("option");
+			option.value = i;
+			option.text = i + " x " + i;
+			select.add(option);	
 		}
-		else {
-			alert("You did not enter a valid number\n\nUsing default 12x12");
-			createGrid(12);
-		}
+	}
 }
 
 function createGrid(size) {
-	var newDiv = "<div class='box'></div>", boxCount = size * size +1;
+	var newDiv = "<div class='box'></div>"
+	console.log(newDiv);
+	var boxCount = size * size +1;
 	// boxsize needs to account for 1px border -> 2n/n = 2
 	var boxSize = (gridWidth / size) - 2;
 	$(".box").remove();
 	while (boxCount -= 1) wrapper.append(newDiv);
-	// Cache selectors and set CSS properties
+	// Cache selectors and set properties
   square = $(".box");
 	square.css({
 		width: boxSize,
@@ -54,7 +59,7 @@ function createGrid(size) {
 }
 
 function normalColor() {
-	$(this).toggleClass("highlight");
+	$(this).css("background-color", "#d3d2d8");
 }
 
 function randomColor(){
@@ -72,8 +77,6 @@ function boxTracer() {
 		$this.fadeTo("fast", 1);
 	});
 }
-
-
 
 
 
