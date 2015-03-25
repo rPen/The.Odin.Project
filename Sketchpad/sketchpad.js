5021a3bd6f5d2c7e105f7f5b7168b71aaa2c4066
Rob Pennington <robpenn002@gmail.com>
2015-03-25 17:49:04 -0500

    refactor grid namespace

--- Sketchpad/sketchpad.js
+++ Sketchpad/sketchpad.js
@@ -1,39 +1,40 @@
+var grid = {
+	wrapper: $(".wrapper"),
+	width: $(".wrapper").width(),
+	square: null
+};
+
 $(document).ready(function() {
-	wrapper = $(".wrapper");
-	gridWidth = wrapper.width();
 	popDropDown("grid",2,32);
 	createGrid(12);
-	wrapper.on("mouseenter", ".box", normalColor);
+	grid.wrapper.on("mouseenter", ".box", normalColor);
 });
 
-var normal = $("#normal");
-normal.on("click", function() {
-	wrapper.off();
-	square.css("background-color", "");
-	wrapper.on("mouseenter", ".box", normalColor);
+$("#normal").on("click", function() {
+	grid.wrapper.off();
+	grid.square.css("background-color", "");
+	grid.wrapper.on("mouseenter", ".box", normalColor);
 });
 
-var random = $("#random");
-random.on("click", function() {
-	wrapper.off();
-	square.css("background-color", "");
-	wrapper.on("mouseenter", ".box", function() {
+$("#random").on("click", function() {
+	grid.wrapper.off();
+	grid.square.css("background-color", "");
+	grid.wrapper.on("mouseenter", ".box", function() {
 		$(this).css("background-color", randomColor());
 	}); 
 }); 
 
-var tracer = $("#tracer");
-tracer.on("click", function() {
-	wrapper.off();
-	square.css("background-color", "");
-	wrapper.on("mouseenter", ".box", boxTracer);
+$("#tracer").on("click", function() {
+	grid.wrapper.off();
+	grid.square.css("background-color", "");
+	grid.wrapper.on("mouseenter", ".box", boxTracer);
 });
 
 function popDropDown(id, min, max) {
 	var i, select, option;
 	select = document.getElementById(id);
 	for (i = min; i <= max; i += 1) {
-		if (gridWidth % i  === 0) {
+		if (grid.width % i  === 0) {
 			option = document.createElement("option");
 			option.value = i;
 			option.text = i + " x " + i;
@@ -44,15 +45,14 @@ function popDropDown(id, min, max) {
 
 function createGrid(size) {
 	var newDiv = "<div class='box'></div>"
-	console.log(newDiv);
-	var boxCount = size * size +1;
+	var boxCount = size * size + 1;
 	// boxsize needs to account for 1px border -> 2n/n = 2
-	var boxSize = (gridWidth / size) - 2;
+	var boxSize = (grid.width / size) - 2;
 	$(".box").remove();
-	while (boxCount -= 1) wrapper.append(newDiv);
+	while (boxCount -= 1) grid.wrapper.append(newDiv);
 	// Cache selectors and set properties
-  square = $(".box");
-	square.css({
+  grid.square = $(".box");
+	grid.square.css({
 		width: boxSize,
 		height: boxSize
 	}); 
