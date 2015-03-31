552278d659dc3712ff07e74d9d50a0326e7b1500
Rob Pennington <robpenn002@gmail.com>
2015-03-31 15:33:05 -0500

    update

--- Sketchpad/sketchpad.js
+++ Sketchpad/sketchpad.js
@@ -1,14 +1,20 @@
 "use strict";
 $(document).ready(function() {
-  sketch.popDropDown("grid", 2, 32);
-  sketch.createGrid(12);
-  sketch.mouseHandler.on("mouseenter", ".box", sketch.normal);
+  sketch.startup();
 });
 
+
 var sketch = (function() { 
+
   var wrapper = $(".wrapper"), width = wrapper.width(), 
       square = null;
  
+  function init() {
+    populateDropDown("grid", 2, 32);
+    createGrid(12);
+    wrapper.on("mouseenter", ".box", normalColor);
+  }
+
   function normalColor() {
     $(this).css("background-color", "#d3d2d8");
   }
@@ -33,70 +39,74 @@ var sketch = (function() {
       $this.css("opacity", $opacity - 0.2);
   }
 
+  function populateDropDown(id, min, max) {
+    var i, select, option;
+    select = document.getElementById(id);
+    for (i = min; i <= max; i += 1) {
+      if (width % i === 0) {
+        option = document.createElement("option");
+        option.value = i;
+        option.text = i + " x " + i;
+        select.add(option);
+      }
+    }
+  }
+
+  function createGrid(size) {
+    // boxsize needs to account for 1px border -> 2n/n = 2
+    var boxSize = (width / size) - 2,
+        boxCount = size * size + 1,
+        newDiv = "<div class='box'></div>";
+        $(".box").remove();
+        while ((boxCount -= 1)) wrapper.append(newDiv);  
+        // Cache selectors and set properties
+        square = $(".box");
+        square.css({
+          width: boxSize,
+          height: boxSize
+        });
+  }
+
+  function resetSquare() {
+    wrapper.off();
+    square.css({
+      "background-color": "",
+      opacity: 1
+    });
+  }
+
   return {
 
+    startup: init,
     mouseHandler: wrapper,
     normal: normalColor,
     random: randomColor,
     tracer: boxTracer,
     shader: boxShader,
-
-    popDropDown: function(id, min, max) {
-    	var i, select, option;
-    	select = document.getElementById(id);
-    	for (i = min; i <= max; i += 1) {
-    		if (width % i  === 0) {
-					option = document.createElement("option");
-					option.value = i;
-					option.text = i + " x " + i;
-					select.add(option);	
-				}
-			}
-		},
-
-    createGrid: function(size) {
-      // boxsize needs to account for 1px border -> 2n/n = 2
-      var boxSize = (width / size) - 2,
-          boxCount = size * size + 1,
-          newDiv = "<div class='box'></div>";
-      $(".box").remove();
-      while ((boxCount -= 1)) wrapper.append(newDiv);
-      // Cache selectors and set properties
-      square = $(".box");
-      square.css({
-        width: boxSize,
-        height: boxSize
-      }); 
-    },
-
-    resetSquare: function() {
-      wrapper.off();
-      square.css({
-        "background-color": "",
-        opacity: 1
-      });
-    }
+    gridDropDown: populateDropDown,
+    build: createGrid,
+    clear: resetSquare
   }; 
 })();
 
 $("#normal").on("click", function() {
-  sketch.resetSquare();
+  sketch.clear();
   sketch.mouseHandler.on("mouseenter", ".box", sketch.normal);
 });
 
 $("#random").on("click", function() {
-  sketch.resetSquare();
+  sketch.clear();
   sketch.mouseHandler.on("mouseenter", ".box", function() {
     $(this).css("background-color", sketch.random);
   }); 
 }); 
 
 $("#tracer").on("click", function() {
-  sketch.resetSquare();
+  sketch.clear();
   sketch.mouseHandler.on("mouseenter", ".box", sketch.tracer);
 });
 
 $("#shader").on("click", function() {
-  sketch.resetSquare();
+  sketch.clear();
   sketch.mouseHandler.on("mouseenter", ".box", sketch.shader);
 });
