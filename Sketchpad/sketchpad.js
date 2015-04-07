94246810326dcacf3a81edef23bcc38e79629d46
Rob Pennington <robpenn002@gmail.com>
2015-04-07 13:08:09 -0500

    continuing to learn
    formatting fix
    
    formatting
    
    formatting

--- Sketchpad/sketchpad.js
+++ Sketchpad/sketchpad.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 var sketch = (function () {
 
-  var $wrapper = $(".wrapper"),
+  var $wrapper = $('.wrapper'),
     width = $wrapper.width(),
     $square = null;
 
   function init() {
-    populateDropDown("grid", 2, 32);
+    populateDropDown('grid', 2, 32);
     createGrid(12);
-    $wrapper.on("mouseenter", ".box", normalColor);
+    $wrapper.on('mouseenter', '.box', normalColor);
   }
 
   function populateDropDown(id, min, max) {
@@ -17,9 +17,9 @@ var sketch = (function () {
     for (i = min; i <= max; i += 1) {
       // grid dimensions must be a factor of 'width'
       if (width % i === 0) {
-        option = document.createElement("option");
+        option = document.createElement('option');
         option.value = i;
-        option.text = i + " x " + i;
+        option.text = i + ' x ' + i;
         select.add(option);
       }
     }
@@ -29,14 +29,14 @@ var sketch = (function () {
     // boxsize needs to account for 1px border -> 2n/n = 2
     var boxSize = (width / size) - 2,
       boxCount = size * size,
-      newDiv = "<div class='box'></div>";
-    $(".box").remove();
+      newDiv = '<div class="box"></div>';
+    $('.box').remove();
     while (boxCount > 0) {
       $wrapper.append(newDiv);
       boxCount -= 1;
     }
     // Cache selectors and set properties
-    $square = $(".box");
+    $square = $('.box');
     $square.css({
       width: boxSize,
       height: boxSize
@@ -44,7 +44,7 @@ var sketch = (function () {
   }
 
   function normalColor() {
-    $(this).css("background-color", "#d3d2d8");
+    $(this).css('background-color', '#d3d2d8');
   }
 
   function randomColor() {
@@ -53,28 +53,28 @@ var sketch = (function () {
       boxRgb[i] = (Math.floor(Math.random() * 256));
     }
 
-    setColor = "rgb(" + boxRgb[0] + "," + boxRgb[1] + "," + boxRgb[2] + ")";
+    setColor = 'rgb(' + boxRgb[0] + ',' + boxRgb[1] + ',' + boxRgb[2] + ')';
 
     return setColor;
   }
 
   function boxTracer() {
     var $this = $(this);
-    $this.fadeTo("slow", 0);
-    $this.fadeTo("fast", 1);
+    $this.fadeTo('slow', 0);
+    $this.fadeTo('fast', 1);
   }
 
   function boxShader() {
-    var $this = $(this), $opacity = $this.css("opacity");
+    var $this = $(this), $opacity = $this.css('opacity');
     if ($opacity > 0.2) {
-      $this.css("opacity", $opacity - 0.2);
+      $this.css('opacity', $opacity - 0.2);
     }
   }
 
   function resetSquare() {
     $wrapper.off();
     $square.css({
-      "background-color": "",
+      'background-color': '',
       opacity: 1
     });
   }
@@ -93,26 +93,26 @@ var sketch = (function () {
   };
 }());
 
-$("#normal").on("click", function () {
+$('#normal').on('click', function () {
   sketch.clear();
-  sketch.mouseHandler.on("mouseenter", ".box", sketch.normal);
+  sketch.mouseHandler.on('mouseenter', '.box', sketch.normal);
 });
 
-$("#random").on("click", function () {
+$('#random').on('click', function () {
   sketch.clear();
-  sketch.mouseHandler.on("mouseenter", ".box", function () {
-    $(this).css("background-color", sketch.random);
+  sketch.mouseHandler.on('mouseenter', '.box', function () {
+    $(this).css('background-color', sketch.random);
   });
 });
 
-$("#tracer").on("click", function () {
+$('#tracer').on('click', function () {
   sketch.clear();
-  sketch.mouseHandler.on("mouseenter", ".box", sketch.tracer);
+  sketch.mouseHandler.on('mouseenter', '.box', sketch.tracer);
 });
 
-$("#shader").on("click", function () {
+$('#shader').on('click', function () {
   sketch.clear();
-  sketch.mouseHandler.on("mouseenter", ".box", sketch.shader);
+  sketch.mouseHandler.on('mouseenter', '.box', sketch.shader);
 });
 
 $(document).ready(function () {
